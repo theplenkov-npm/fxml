b18285e1171aff570b00aea8c60dce645eacaad4
Petr Plenkov <petr.plenkov@gmail.com>
2024-11-13 14:13:29 +0000

    test auto-format

--- packages/fxml/src/lib/attr.ts
+++ packages/fxml/src/lib/attr.ts
@@ -1,16 +1,15 @@
 const ATTR_PREFIX = '@_' as const;
-
 type AttrKeys<T> = {
-    [K in keyof T as `${typeof ATTR_PREFIX}${string & K}`]: T[K];
+  [K in keyof T as `${typeof ATTR_PREFIX}${string & K}`]: T[K];
 };
 
 export function $attr<T extends Record<string, any>>(object: T): AttrKeys<T> {
-    const result: Record<string, any> = {};
+  const result: Record<string, any> = {};
 
-    for (const [key, value] of Object.entries(object)) {
-        const attrKey = `${ATTR_PREFIX}${key}`;
-        result[attrKey] = value;
-    }
+  for (const [key, value] of Object.entries(object)) {
+    const attrKey = `${ATTR_PREFIX}${key}`;
+    result[attrKey] = value;
+  }
 
-    return result as AttrKeys<T>;
+  return result as AttrKeys<T>;
 }
